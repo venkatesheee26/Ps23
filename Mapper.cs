eab620a21c8e4e726745fbb56ed4124fd76b2fe8
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-08-07 08:42:44 +0530

    ps23

---
+++ Mapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_22
+{
+    public class Mapper
+    {
+        public static void MapProperties(Source source, Destination destination)
+        {
+            // Get the types of Source and Destination classes
+            Type sourceType = typeof(Source);
+            Type destType = typeof(Destination);
+
+            // Get all properties of the Source class
+            PropertyInfo[] sourceProperties = sourceType.GetProperties();
+
+            // Loop through the properties and check if they exist in the Destination class
+            foreach (PropertyInfo sourceProperty in sourceProperties)
+            {
+                PropertyInfo destProperty = destType.GetProperty(sourceProperty.Name);
+                if (destProperty != null && destProperty.CanWrite)
+                {
+                    // Get the value of the property from Source and set it in Destination
+                    object value = sourceProperty.GetValue(source);
+                    destProperty.SetValue(destination, value);
+                }
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_22
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            // Create instances of Source and Destination classes
+            Source source = new Source
+            {
+                Id = 1,
+                Name = "John Doe",
+                Age = 30,
+                Address = "123 Main St"
+            };
+
+            Destination destination = new Destination
+            {
+                Id = 0, // This value will be updated through dynamic property mapping
+                Name = "Default", // This value will be updated through dynamic property mapping
+                Age = 0, // This value will be updated through dynamic property mapping
+                City = "New York",
+                PhoneNumber = "22555-1234"
+            };
+
+            // Call the MapProperties method to map properties from Source to Destination
+            Mapper.MapProperties(source, destination);
+
+            // Display the values of the properties in the Destination class
+            Console.WriteLine("Destination Properties after Mapping:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Age: {destination.Age}");
+            Console.WriteLine($"City: {destination.City}");
+            Console.WriteLine($"PhoneNumber: {destination.PhoneNumber}");
+        }
+    }
+
+}
